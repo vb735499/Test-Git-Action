0ce2bf74af4207c0d95b027549180beb9862c4e2
kevin.macbook <vb735499@gmail.com>
2024-05-03 05:30:04 +0800

    modify main.go

--- main.go
+++ main.go
@@ -9,4 +9,5 @@ func main() {
 	log.Println("I'm master your not.")
 	log.Println("Okay. Now is Dockerfile time.")
 	log.Println("Don't forget to check your username/password.")
+	log.Println("Oops. it's look like ocurr some problem with permission.")
 }
