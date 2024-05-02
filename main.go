0612e604cbb679aab42736c9f6e4b33140039837
kevin.macbook <vb735499@gmail.com>
2024-05-03 05:20:51 +0800

    modify main.go

--- main.go
+++ main.go
@@ -8,4 +8,5 @@ func main() {
 	log.Println("Hello world")
 	log.Println("I'm master your not.")
 	log.Println("Okay. Now is Dockerfile time.")
+	log.Println("Don't forget to check your username/password.")
 }
